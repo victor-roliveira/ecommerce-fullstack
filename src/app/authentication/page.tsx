585d8d253746b6e5fdbea216cc2f21bfacbd6d00
victor-roliveira <victorramos123@outlook.com>
2025-09-04 21:05:44 -0300

    feat: add sheet component to user login session

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,23 +1,28 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SignInForm from "./components/sign-in-form";
 import SignUpForm from "./components/sign-up-form";
+import { Header } from "@/components/common/header";
 
 const Authentication = async () => {
   return (
-    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
-      <Tabs defaultValue="account">
-        <TabsList>
-          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
-          <TabsTrigger value="sign-up">Criar Conta</TabsTrigger>
-        </TabsList>
-        <TabsContent value="sign-in">
-          <SignInForm />
-        </TabsContent>
-        <TabsContent value="sign-up">
-          <SignUpForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <>
+      <Header />
+
+      <div className="flex w-full flex-col items-center gap-6 p-5">
+        <Tabs defaultValue="account">
+          <TabsList>
+            <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+            <TabsTrigger value="sign-up">Criar Conta</TabsTrigger>
+          </TabsList>
+          <TabsContent value="sign-in">
+            <SignInForm />
+          </TabsContent>
+          <TabsContent value="sign-up">
+            <SignUpForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 };
 
